4fa2710756ff6645a0219fa5c25903b7c7ab72fd
Johanne <johannegi16@ru.is>
2018-09-28 15:16:29 +0000

    client

--- client.cpp
+++ client.cpp
@@ -15,7 +15,7 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, port1, port2, port3, n;
+    int sockfd1, sockfd2, sockfd3, port1, port2, port3, n;
     struct sockaddr_in serv_addr;           // Socket address structure
     struct hostent *server;
 
@@ -26,12 +26,14 @@ int main(int argc, char *argv[])
     }
 
     port1 = atoi(argv[2]);     // Read Port No from command line
-    //port2 = atoi(argv[3]); 
-    //port3 = atoi(argv[4]); 
+    port2 = atoi(argv[3]); 
+    port3 = atoi(argv[4]); 
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); // Open Socket
+    sockfd1 = socket(AF_INET, SOCK_STREAM, 0); // Open Socket
+    sockfd2 = socket(AF_INET, SOCK_STREAM, 0); // Open Socket
+    sockfd3 = socket(AF_INET, SOCK_STREAM, 0); // Open Socket
 
-    if (sockfd < 0) 
+    if (sockfd1 < 0 || sockfd2 < 0 || sockfd3 < 0) 
         error("ERROR opening socket");
 
     server = gethostbyname(argv[1]);        // Get host from IP
@@ -52,23 +54,46 @@ int main(int argc, char *argv[])
 
     serv_addr.sin_port = htons(port1);
 
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    if (connect(sockfd1,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    { 
         error("ERROR connecting");
+    }
+
+    close(sockfd1);
+    serv_addr.sin_port = htons(port2);
+
+    if (connect(sockfd2,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    {
+        error("ERROR connecting");
+    }
+    
+    close(sockfd2);
+    serv_addr.sin_port = htons(port3);
+
+    if (connect(sockfd3,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    {
+        error("ERROR connecting");
+    }
         
     // Read and write to socket
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
+    while(true)
+    {
+        printf("Please enter the message: ");
+        bzero(buffer,256);
+        fgets(buffer,255,stdin);
+        n = write(sockfd3,buffer,strlen(buffer));
 
-    if (n < 0) 
-         error("ERROR writing to socket");
-
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
+        if (n < 0) 
+        {
+            error("ERROR writing to socket");
+        }
+    }
+    
+    /*bzero(buffer,256);
+    n = read(sockfd3,buffer,255);
     if (n < 0) 
          error("ERROR reading from socket");
     printf("%s\n",buffer);
-    close(sockfd);
+    close(sockfd3);*/
     return 0;
 }
