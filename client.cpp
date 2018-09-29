3b983357c105d1051f318b7879c3680290490f7a
peddilama <pedro16@ru.is>
2018-09-29 07:39:25 +0000

    yooy

--- client.cpp
+++ client.cpp
@@ -87,13 +87,13 @@ int main(int argc, char *argv[])
         {
             error("ERROR writing to socket");
         }
+        bzero(buffer,256);
+        n = read(sockfd3,buffer,255);
+        if (n < 0) 
+            error("ERROR reading from socket");
+        printf("%s\n",buffer);
     }
     
-    /*bzero(buffer,256);
-    n = read(sockfd3,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd3);*/
+    
     return 0;
 }
