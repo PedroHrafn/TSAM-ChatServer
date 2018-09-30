74fed3cc28f93ff74589f970b72b5cb96d71ef90
Peddiprump <kristoferu16@ru.is>
2018-09-30 16:43:11 +0000

    ID og CHANGE_ID komið

--- server.cpp
+++ server.cpp
@@ -14,8 +14,8 @@
 #include <sstream>
 #include <ctime>
 
-#define PORT    5555
-#define MAXMSG  512
+#define PORT 5555
+#define MAXMSG 512
 
 int rightInARow;
 
@@ -23,7 +23,7 @@ int FIRSTKNOCK = 5553;
 int SECONDKNOCK = 5554;
 int LASTPORT = 5555;
 
-std::string SERVER_ID = "Group 18";
+std::string SERVER_ID;
 
 /*  This will probably not be used
 struct chat_user
@@ -34,33 +34,32 @@ struct chat_user
 };
 std::vector<chat_user> chat_users; */
 
-
 // A map of username and it's corresponding FD
 std::map<std::string, int> logged_users;
 // A map of an IP address and it's corresponding correct knocks in a row
 std::map<std::string, int> correct_knocks;
 
-int make_socket (uint16_t port)
+int make_socket(uint16_t port)
 {
   int sock;
   struct sockaddr_in name;
 
   /* Create the socket. */
-  sock = socket (PF_INET, SOCK_STREAM, 0);
+  sock = socket(PF_INET, SOCK_STREAM, 0);
   if (sock < 0)
-    {
-      perror ("socket");
-      exit (EXIT_FAILURE);
-    }
+  {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
 
   /* Give the socket a name. */
   name.sin_family = AF_INET;
-  name.sin_port = htons (port);
-  name.sin_addr.s_addr = htonl (INADDR_ANY);
-  if (bind (sock, (struct sockaddr *) &name, sizeof (name)) < 0)
+  name.sin_port = htons(port);
+  name.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind(sock, (struct sockaddr *)&name, sizeof(name)) < 0)
   {
-    perror ("bind");
-    exit (EXIT_FAILURE);
+    perror("bind");
+    exit(EXIT_FAILURE);
   }
 
   return sock;
@@ -74,144 +73,23 @@ int send_message(int userSock, std::string message)
 
 std::string get_user_by_fd(int userSock)
 {
-  for (std::map<std::string,int>::iterator it=logged_users.begin(); it!=logged_users.end(); ++it)
+  for (std::map<std::string, int>::iterator it = logged_users.begin(); it != logged_users.end(); ++it)
   {
-    if(it->second != userSock)
+    if (it->second != userSock)
     {
       return it->first;
     }
   }
   return "";
 }
-
-int read_from_client (int userSock)
+std::string get_id()
 {
-  char buffer[MAXMSG] = {};
-  int nbytes;
-
-  nbytes = read (userSock, buffer, MAXMSG);
-  if (nbytes < 0)
-    {
-      /* Read error. */
-      perror ("read");
-      exit (EXIT_FAILURE);
-    }
-  else if (nbytes == 0)
-    /* End-of-file. */
-    return -1;
-  else
-    {
-      /* Data read. */
-      fprintf (stderr, "Server: got message: %s\n", buffer);
-      //std::string msg = "kalli\n";
-      //send_message(filedes, msg);
-      std::string s = buffer;
-      s.erase(s.size() - 1);
-      size_t splitter = s.find(' ');
-      std::string command = s.substr(0, splitter);
-
-      if(splitter == std::string::npos || splitter == s.size() - 1)
-      {
-        //send_message(userSock, "TYPE COMMAND, SPACE, SOMETHING");
-        //return 0;
-      }
-      std::string message = s.substr(splitter + 1);
-
-      if(command == "WHO")
-      {
-        send_message(userSock, "\nUsers logged in the chatroom:");
-        for (auto it=logged_users.begin(); it!=logged_users.end(); ++it)
-          send_message(userSock, it->first);
-      }
-      else if(command == "LEAVE")
-      {
-        for (std::map<std::string,int>::iterator it=logged_users.begin(); it!=logged_users.end(); ++it)
-          {
-            if(it->second == userSock)
-            {
-              logged_users.erase(it);
-              return -1;
-            }
-          }
-        return -1;
-      }
-      else if(command == "CONNECT")
-      {
-        std::string username = message.substr(0, splitter);
-        auto checkUsername = logged_users.find(username);
-        if(checkUsername == logged_users.end())
-        {
-          for (auto it=logged_users.begin(); it!=logged_users.end(); ++it)
-          {
-            if(it->second == userSock)
-            {
-              fprintf(stderr, "You are already connected\n");
-              send_message(userSock, "You are already connected as: " + it->first);
-              return 0;
-            }
-          }
-          logged_users.insert(std::pair<std::string,int>(username,userSock));
-          send_message(userSock, "Joined server successfully");
-          fprintf(stderr, "success\n");
-          
-        }
-        else 
-        {
-          std::string msgg = "Username taken";
-          fprintf(stderr, "Username taken\n");
-          send_message(userSock, msgg);
-        }
-      }
-      else
-      {
-        for (std::map<std::string,int>::iterator it=logged_users.begin(); it!=logged_users.end(); ++it)
-          {
-            std::string username;
-            if(it->second == userSock)
-            {
-              username = it->first;
-              if(command == "MSG_ALL")
-              {
-                for (std::map<std::string,int>::iterator it=logged_users.begin(); it!=logged_users.end(); ++it)
-                {
-                  if(it->second != userSock)
-                  {
-                    message = username + ": " + message;
-                    send_message(it->second, message);
-                  }
-                }
-              }
-              else if(command == "MSG")
-              {
-                int splitter = message.find(' ');
-                std::string reciever = message.substr(0, splitter);
-                std::string message_to_rec = message.substr(splitter + 1);
-                auto recSock = logged_users.find(reciever);
-                if(recSock != logged_users.end())
-                  send_message(recSock->second, message_to_rec);
-                else
-                  send_message(userSock, "User with name '" + reciever + "' not found");
-              }
-              return 0;
-            }
-          }
-          send_message(userSock, "You must connect first to use commands");
-      }
-      
-      return 0;
-    }
+  return SERVER_ID;
 }
-
-void changePorts()
-{
-  FIRSTKNOCK = 5556;
-  SECONDKNOCK = 5557;
-  LASTPORT = 5558;
-}
-
 void generate_id()
 {
-  std::time_t time = std::time(nullptr); 
+  SERVER_ID = "Group 18";
+  std::time_t time = std::time(nullptr);
   std::stringstream ss;
   ss << time;
   std::string time_stamp = ss.str().append(" ");
@@ -222,22 +100,154 @@ void generate_id()
   std::array<char, 256> buffer;
   std::string result;
 
-  FILE* pipe = popen(command.c_str(), "r"); 
+  FILE *pipe = popen(command.c_str(), "r");
 
-  while (fgets(buffer.data(), 256, pipe) != NULL) {
+  while (fgets(buffer.data(), 256, pipe) != NULL)
+  {
     result += buffer.data();
   }
 
   pclose(pipe);
+  // correctly formats the id string
   SERVER_ID.insert(0, result);
 }
 
-std::string get_id()
+int read_from_client(int userSock)
 {
-  return SERVER_ID;
+  char buffer[MAXMSG] = {};
+  int nbytes;
+
+  nbytes = read(userSock, buffer, MAXMSG);
+  if (nbytes < 0)
+  {
+    /* Read error. */
+    perror("read");
+    exit(EXIT_FAILURE);
+  }
+  else if (nbytes == 0)
+    /* End-of-file. */
+    return -1;
+  else
+  {
+    /* Data read. */
+    fprintf(stderr, "Server: got message: %s\n", buffer);
+    //std::string msg = "kalli\n";
+    //send_message(filedes, msg);
+    std::string s = buffer;
+    s.erase(s.size() - 1);
+    size_t splitter = s.find(' ');
+    std::string command = s.substr(0, splitter);
+
+    if (splitter == std::string::npos || splitter == s.size() - 1)
+    {
+      //send_message(userSock, "TYPE COMMAND, SPACE, SOMETHING");
+      //return 0;
+    }
+    std::string message = s.substr(splitter + 1);
+
+    if (command == "ID")
+    {
+      std::string print_id = get_id();
+      std::cout << print_id << std::endl;
+    }
+    if (command == "WHO")
+    {
+      send_message(userSock, "\nUsers logged in the chatroom:");
+      for (auto it = logged_users.begin(); it != logged_users.end(); ++it)
+        send_message(userSock, it->first);
+    }
+    if (command == "CHANGE_ID")
+    {
+      generate_id();
+      SERVER_ID = get_id();
+    }
+    else if (command == "LEAVE")
+    {
+      for (std::map<std::string, int>::iterator it = logged_users.begin(); it != logged_users.end(); ++it)
+      {
+        if (it->second == userSock)
+        {
+          logged_users.erase(it);
+          return -1;
+        }
+      }
+      return -1;
+    }
+    else if (command == "CONNECT")
+    {
+      std::string username = message.substr(0, splitter);
+      auto checkUsername = logged_users.find(username);
+      if (checkUsername == logged_users.end())
+      {
+        for (auto it = logged_users.begin(); it != logged_users.end(); ++it)
+        {
+          if (it->second == userSock)
+          {
+            fprintf(stderr, "You are already connected\n");
+            send_message(userSock, "You are already connected as: " + it->first);
+            return 0;
+          }
+        }
+        logged_users.insert(std::pair<std::string, int>(username, userSock));
+        send_message(userSock, "Joined server successfully");
+        fprintf(stderr, "success\n");
+      }
+      else
+      {
+        std::string msgg = "Username taken";
+        fprintf(stderr, "Username taken\n");
+        send_message(userSock, msgg);
+      }
+    }
+    else
+    {
+      for (std::map<std::string, int>::iterator it = logged_users.begin(); it != logged_users.end(); ++it)
+      {
+        std::string username;
+        if (it->second == userSock)
+        {
+          username = it->first;
+          if (command == "MSG_ALL")
+          {
+            for (std::map<std::string, int>::iterator it = logged_users.begin(); it != logged_users.end(); ++it)
+            {
+              if (it->second != userSock)
+              {
+                message = username + ": " + message;
+                send_message(it->second, message);
+              }
+            }
+          }
+          else if (command == "MSG")
+          {
+            int splitter = message.find(' ');
+            std::string reciever = message.substr(0, splitter);
+            std::string message_to_rec = message.substr(splitter + 1);
+            auto recSock = logged_users.find(reciever);
+            if (recSock != logged_users.end())
+              send_message(recSock->second, message_to_rec);
+            else
+              send_message(userSock, "User with name '" + reciever + "' not found");
+          }
+          return 0;
+        }
+      }
+      send_message(userSock, "You must connect first to use commands");
+    }
+
+    return 0;
+  }
 }
 
-int main (void)
+void changePorts()
+{
+  FIRSTKNOCK = 5556;
+  SECONDKNOCK = 5557;
+  LASTPORT = 5558;
+}
+
+
+int main(void)
 {
   //changePorts();
   int sock, firstKnockSock, secondKnockSock;
@@ -246,90 +256,87 @@ int main (void)
   struct sockaddr_in clientname;
   size_t size;
 
-
-  
   generate_id();
-  std::cout <<SERVER_ID << std::endl;
 
   /* Create the socket and set it up to accept connections. */
-  firstKnockSock = make_socket (FIRSTKNOCK);
-  secondKnockSock = make_socket (SECONDKNOCK);
-  sock = make_socket (LASTPORT);
+  firstKnockSock = make_socket(FIRSTKNOCK);
+  secondKnockSock = make_socket(SECONDKNOCK);
+  sock = make_socket(LASTPORT);
 
-  if (listen (sock, 1) < 0 || listen (secondKnockSock, 1) < 0 || listen (firstKnockSock, 1) < 0)
+  if (listen(sock, 1) < 0 || listen(secondKnockSock, 1) < 0 || listen(firstKnockSock, 1) < 0)
   {
-    perror ("listen");
-    exit (EXIT_FAILURE);
+    perror("listen");
+    exit(EXIT_FAILURE);
   }
 
   /* Initialize the set of active sockets. */
-  FD_ZERO (&active_fd_set);
-  FD_SET (firstKnockSock, &active_fd_set);
-  FD_SET (sock, &active_fd_set);
-  FD_SET (secondKnockSock, &active_fd_set);
+  FD_ZERO(&active_fd_set);
+  FD_SET(firstKnockSock, &active_fd_set);
+  FD_SET(sock, &active_fd_set);
+  FD_SET(secondKnockSock, &active_fd_set);
 
   while (1)
   {
     /* Block until input arrives on one or more active sockets. */
     read_fd_set = active_fd_set;
-    if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
+    if (select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
     {
-      perror ("select");
-      exit (EXIT_FAILURE);
+      perror("select");
+      exit(EXIT_FAILURE);
     }
 
     /* Service all the sockets with input pending. */
     for (i = 0; i < FD_SETSIZE; ++i)
-      if (FD_ISSET (i, &read_fd_set))
+      if (FD_ISSET(i, &read_fd_set))
       {
         /* Checked what socket has input pending */
         if (i == firstKnockSock || i == secondKnockSock || i == sock)
         {
           int newSock;
-          size = sizeof (clientname);
-          newSock = accept (i, (struct sockaddr *) &clientname, (socklen_t*)&size);
-          std::string IP_addr = inet_ntoa (clientname.sin_addr);
+          size = sizeof(clientname);
+          newSock = accept(i, (struct sockaddr *)&clientname, (socklen_t *)&size);
+          std::string IP_addr = inet_ntoa(clientname.sin_addr);
           auto knock = correct_knocks.find(IP_addr);
           if (i == firstKnockSock)
           {
             close(newSock);
-            if(knock == correct_knocks.end())
-              correct_knocks.insert( std::pair<std::string,int>(IP_addr,1) );
+            if (knock == correct_knocks.end())
+              correct_knocks.insert(std::pair<std::string, int>(IP_addr, 1));
             else
               knock->second = 1;
           }
           else if (i == secondKnockSock)
           {
             close(newSock);
-            if(knock != correct_knocks.end() && knock->second == 1)
+            if (knock != correct_knocks.end() && knock->second == 1)
               knock->second++; // knock sucess
-            else if(knock != correct_knocks.end())
+            else if (knock != correct_knocks.end())
               correct_knocks.erase(knock); // wrong port knock
           }
           else
           {
-            if(knock == correct_knocks.end() || knock->second != 2)
+            if (knock == correct_knocks.end() || knock->second != 2)
               close(newSock);
             else
             {
               /* Knocking succeeded and FD is set for the new socket */
-              FD_SET (newSock, &active_fd_set);
+              FD_SET(newSock, &active_fd_set);
               // TODO: insert to logged_users
               //logged_users.insert( std::pair<std::string,int>(USERNAME, newSock?));
             }
-            if(knock != correct_knocks.end())
+            if (knock != correct_knocks.end())
               correct_knocks.erase(knock);
           }
         }
-        
+
         else
         {
           /* Data arriving on an already-connected socket. */
-          if (read_from_client (i) < 0)
+          if (read_from_client(i) < 0)
           {
             // TODO: delete user from logged_users
-            close (i);
-            FD_CLR (i, &active_fd_set);
+            close(i);
+            FD_CLR(i, &active_fd_set);
           }
         }
       }
